512d52ce2e4b35de85bde8174ce5c0f8255dc300
miooo0o <112120518+miooo0o@users.noreply.github.com>
2023-05-21 16:14:06 +0200

    start

---
+++ push_swap.h
@@ -0,0 +1,12 @@
+# ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "libft.h"
+# include "ft_printf.h"
+
+/* library */
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+
+# endif
