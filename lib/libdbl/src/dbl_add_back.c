cdfc341c2f7d02746c5b3332ddcc84d98735fc99
Mina Kim <mmmiiinn@gmail.com>
2023-06-18 13:04:11 +0200

    #upsate : TODO, FIXME

--- lib/libdbl/src/dbl_add_back.c
+++ lib/libdbl/src/dbl_add_back.c
@@ -6,7 +6,7 @@
 /*   By: minakim <minakim@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 14:10:36 by minakim           #+#    #+#             */
-/*   Updated: 2023/06/05 22:52:23 by minakim          ###   ########.fr       */
+/*   Updated: 2023/06/18 13:04:07 by minakim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,24 +30,3 @@ void	dbl_add_back(t_lst *list, t_doubly **new_node)
 		list->last = *new_node;
 	}
 }
-
-/*
-int dbl_add_back(t_doubly **node, t_doubly *new_node)
-{
-	t_doubly *temp;
-
-	if (node == NULL || new_node == NULL)
-		return (-1);
-	if (*node == NULL)
-	{
-		new_node->prev = NULL;
-		*node = new_node;
-	}
-	else
-	{
-		temp = dbl_move_last((*node));
-		temp->next = new_node;
-		new_node->prev = temp;
-	}
-	return (0);
-}*/
--- lib/libdbl/src/dbl_add_front.c
+++ lib/libdbl/src/dbl_add_front.c
@@ -6,7 +6,7 @@
 /*   By: minakim <minakim@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 14:10:36 by minakim           #+#    #+#             */
-/*   Updated: 2023/06/13 18:02:59 by minakim          ###   ########.fr       */
+/*   Updated: 2023/06/18 13:04:02 by minakim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,15 +32,3 @@ void	dbl_add_front(t_lst *list, t_doubly **new_node)
 		list->head = *new_node;
 	}
 }
-
-/*
-int dbl_add_front(t_doubly **head_node,t_doubly *new_node) {
-	if (head_node == NULL || new_node == NULL)
-		return (-1);
-	new_node->next = *head_node;
-	if (*head_node != NULL)
-		(*head_node)->prev = new_node;
-	new_node->prev = NULL;
-	*head_node = new_node;
-	return (0);
-}*/
